36e21bc9e6fecfa5ca446c753f1a08a305d441a2
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-02-27 18:48:34 +0100

    Implement Rule of Three Refactoring

--- tests/stats_calculator.test.ts
+++ tests/stats_calculator.test.ts
@@ -1,28 +1,26 @@
 import { StatsCalculator } from './../src/stats_calculator';
 
 describe('Stats Calculator', () => {
-  test('should be able to determine the minimum value from the list of integers', () => {
-    const stats = [2, 4, 21, -8, 53, 40];
-    const statsCalculator = new StatsCalculator(stats);
+  const stats = [2, 4, 21, -8, 53, 40];
+  let statsCalculator: StatsCalculator;
 
+  beforeEach(() => {
+    statsCalculator = new StatsCalculator(stats);
+  });
+
+  test('should be able to determine the minimum value from the list of integers', () => {
     const result = statsCalculator.minimumValue();
 
     expect(result).toBe<number>(-8);
   });
 
   test('should be able to determine the maximum value from the list of integers', () => {
-    const stats = [2, 4, 21, -8, 53, 40];
-    const statsCalculator = new StatsCalculator(stats);
-
     const result = statsCalculator.maximumValue();
 
     expect(result).toBe<number>(53);
   });
 
   test('should be able to determine the number of elements in the sequence', () => {
-    const stats = [2, 4, 21, -8, 53, 40];
-    const statsCalculator = new StatsCalculator(stats);
-
     expect(statsCalculator.numberOfElements()).toBe<number>(6);
   });
 });
