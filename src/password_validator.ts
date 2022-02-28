ce323d86e0e426e4e9e2e8eb254e65a6f2335b09
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-02-28 19:28:56 +0100

    Completed the password validation testing

--- src/password_validator.ts
+++ src/password_validator.ts
@@ -3,17 +3,47 @@ export interface ErrorValidator {
   errors: string[];
 }
 
-const result: ErrorValidator = {
-  isValid: true,
-  errors: [],
-};
-
 export class PasswordValidator {
   validate(password: string): ErrorValidator {
-    // if (password.length >= 5 && password.length <= 15) {
-    //   return { ...result, isValid: true };
-    // }
+    let response: ErrorValidator = { isValid: false, errors: [] };
 
-    return { ...result, isValid: true };
+    const parseError = (
+      response: ErrorValidator,
+      msg: string,
+    ): ErrorValidator => {
+      return {
+        isValid: false,
+        errors: [...response.errors, msg],
+      };
+    };
+
+    if (password.length >= 5 && password.length <= 15) {
+      response = { ...response, isValid: true };
+    } else {
+      response = parseError(
+        response,
+        'password length must be between 5 to 15 chars long',
+      );
+    }
+
+    if (password.match(/\d+/g) != null) {
+      response = { ...response, isValid: true };
+    } else {
+      response = parseError(
+        response,
+        'password must contain at least one digit',
+      );
+    }
+
+    if (password.match(/["A-Z"]/)) {
+      response = { ...response, isValid: true };
+    } else {
+      response = parseError(
+        response,
+        'password must contain at least one upper case letter',
+      );
+    }
+
+    return response;
   }
 }
--- tests/password_validator.test.ts
+++ tests/password_validator.test.ts
@@ -1,17 +1,32 @@
-import { ErrorValidator, PasswordValidator } from './../src/password_validator';
+import { PasswordValidator } from './../src/password_validator';
 
 describe('Passsword Validator', () => {
+  let passwordValidator: PasswordValidator;
+  let password: string;
+
+  beforeEach(() => {
+    passwordValidator = new PasswordValidator();
+    password = 'tesyu6Z';
+  });
+
   test('should detect if password is between 5 and 15 characters long', () => {
-    const passwordValidator = new PasswordValidator();
-    const result: ErrorValidator = {
+    expect(passwordValidator.validate(password)).toMatchObject({
       isValid: true,
       errors: [],
-    };
+    });
+  });
 
-    const password = 'testtouou';
+  test('should detect if password contains at least one digit', () => {
+    expect(passwordValidator.validate(password)).toMatchObject({
+      isValid: true,
+      errors: [],
+    });
+  });
 
-    expect(passwordValidator.validate(password)).toMatchObject<ErrorValidator>(
-      result,
-    );
+  test('should detect if password contains at least one upper case letter', () => {
+    expect(passwordValidator.validate(password)).toMatchObject({
+      isValid: true,
+      errors: [],
+    });
   });
 });
