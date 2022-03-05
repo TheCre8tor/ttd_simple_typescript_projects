88acb40522716b2d7cf7b0297ce7337b80ef529f
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-03-05 18:18:21 +0100

    Perform the Rule of Three (duplication)

--- tests/fizzbuzz_tpp.test.ts
+++ tests/fizzbuzz_tpp.test.ts
@@ -1,25 +1,25 @@
 import { FizzBuzzTPP } from './../src/fizzbuzz_tpp';
 
 describe('FizzBuzz', () => {
-  it('should throw an error if number is less than 1', () => {
-    const fizzbuzz = new FizzBuzzTPP();
+  let fizzbuzz: FizzBuzzTPP;
 
+  beforeEach(() => {
+    fizzbuzz = new FizzBuzzTPP();
+  });
+
+  it('should throw an error if number is less than 1', () => {
     const result = (): string => fizzbuzz.run(-2);
 
     expect(result).toThrow(Error);
   });
 
   it('should throw an error if number is greater than 100', () => {
-    const fizzbuzz = new FizzBuzzTPP();
-
     const result = (): string => fizzbuzz.run(101);
 
     expect(result).toThrowError();
   });
 
   it('should return "Fizz" if number is divisible by 3', () => {
-    const fizzbuzz = new FizzBuzzTPP();
-
     const result = fizzbuzz.run(6);
 
     expect(result).toBe('Fizz');
